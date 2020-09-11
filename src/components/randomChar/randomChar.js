b5c2778744a57e62b9c3d5e08630cb14a8dccd0b
Кирилюк <kk87.work@gmail.com>
2020-09-11 13:46:43 +0300

    add spinner and error

--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import "./randomChar.css";
 import gotService from "../../services/gotService";
-
+import Spinner from "../spinner";
+import ErrorMessage from "../errorMessage";
 export default class RandomChar extends Component {
   constructor() {
     super();
@@ -11,43 +12,71 @@ export default class RandomChar extends Component {
   gotService = new gotService();
   state = {
     character: {},
+    loading: true,
   };
 
   onCharacterLoaded = (character) => {
-    this.setState({ character });
+    this.setState({
+      character,
+      loading: false,
+      error: false,
+    });
+  };
+
+  onError = (err) => {
+    this.setState({
+      error: true,
+      loading: false,
+    });
   };
 
   updateCharacter() {
     const id = Math.floor(Math.random() * 140 + 25);
-    this.gotService.getCharacterById(id).then(this.onCharacterLoaded);
+    this.gotService
+      .getCharacterById(id)
+      .then(this.onCharacterLoaded)
+      .catch(this.onError);
   }
 
   render() {
-    const {
-      character: { name, gender, born, died, culture },
-    } = this.state;
+    const { character, loading, error } = this.state;
+    const errorMessage = error ? <ErrorMessage /> : null;
+    const spinner = loading ? <Spinner /> : null;
+    const content = !(loading || error) ? <View character={character} /> : null;
+
     return (
       <div className="random-block rounded">
-        <h4>Random Character: {name}</h4>
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item d-flex justify-content-between">
-            <span className="term">Gender </span>
-            <span>{gender}</span>
-          </li>
-          <li className="list-group-item d-flex justify-content-between">
-            <span className="term">Born </span>
-            <span>{born}</span>
-          </li>
-          <li className="list-group-item d-flex justify-content-between">
-            <span className="term">Died </span>
-            <span>{died}</span>
-          </li>
-          <li className="list-group-item d-flex justify-content-between">
-            <span className="term">Culture </span>
-            <span>{culture}</span>
-          </li>
-        </ul>
+        {errorMessage}
+        {spinner}
+        {content}
       </div>
     );
   }
 }
+
+const View = ({ character }) => {
+  const { name, gender, born, died, culture } = character;
+  return (
+    <>
+      <h4>Random Character: {name}</h4>
+      <ul className="list-group list-group-flush">
+        <li className="list-group-item d-flex justify-content-between">
+          <span className="term">Gender </span>
+          <span>{gender}</span>
+        </li>
+        <li className="list-group-item d-flex justify-content-between">
+          <span className="term">Born </span>
+          <span>{born}</span>
+        </li>
+        <li className="list-group-item d-flex justify-content-between">
+          <span className="term">Died </span>
+          <span>{died}</span>
+        </li>
+        <li className="list-group-item d-flex justify-content-between">
+          <span className="term">Culture </span>
+          <span>{culture}</span>
+        </li>
+      </ul>
+    </>
+  );
+};
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./spinner.css";
+
+const Spinner = () => {
+  return (
+    <div className="loadingio-spinner-spin-fpjov2rj1hc">
+      <div className="ldio-u8rc5hu2avj">
+        <div>
+          <div />
+        </div>
+        <div>
+          <div />
+        </div>
+        <div>
+          <div />
+        </div>
+        <div>
+          <div />
+        </div>
+        <div>
+          <div />
+        </div>
+        <div>
+          <div />
+        </div>
+        <div>
+          <div />
+        </div>
+        <div>
+          <div />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
