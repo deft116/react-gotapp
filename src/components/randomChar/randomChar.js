c176251697ad8f55d8630a67acb41b9ea1aff714
Кирилюк <kk87.work@gmail.com>
2020-09-11 09:17:14 +0300

    random char

--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -1,32 +1,53 @@
-import React, {Component} from 'react';
-import './randomChar.css';
+import React, { Component } from "react";
+import "./randomChar.css";
+import gotService from "../../services/gotService";
 
 export default class RandomChar extends Component {
+  constructor() {
+    super();
+    this.updateCharacter();
+  }
 
-    render() {
+  gotService = new gotService();
+  state = {
+    character: {},
+  };
 
-        return (
-            <div className="random-block rounded">
-                <h4>Random Character: John</h4>
-                <ul className="list-group list-group-flush">
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Gender </span>
-                        <span>male</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Born </span>
-                        <span>11.03.1039</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Died </span>
-                        <span>13.09.1089</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Culture </span>
-                        <span>Anarchy</span>
-                    </li>
-                </ul>
-            </div>
-        );
-    }
+  onCharacterLoaded = (character) => {
+    this.setState({ character });
+  };
+
+  updateCharacter() {
+    const id = Math.floor(Math.random() * 140 + 25);
+    this.gotService.getCharacterById(id).then(this.onCharacterLoaded);
+  }
+
+  render() {
+    const {
+      character: { name, gender, born, died, culture },
+    } = this.state;
+    return (
+      <div className="random-block rounded">
+        <h4>Random Character: {name}</h4>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item d-flex justify-content-between">
+            <span className="term">Gender </span>
+            <span>{gender}</span>
+          </li>
+          <li className="list-group-item d-flex justify-content-between">
+            <span className="term">Born </span>
+            <span>{born}</span>
+          </li>
+          <li className="list-group-item d-flex justify-content-between">
+            <span className="term">Died </span>
+            <span>{died}</span>
+          </li>
+          <li className="list-group-item d-flex justify-content-between">
+            <span className="term">Culture </span>
+            <span>{culture}</span>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
---
+++ src/services/gotService.js
@@ -0,0 +1,75 @@
+export default class GotService {
+  constructor() {
+    this._apiBase = "https://www.anapioficeandfire.com/api";
+  }
+
+  async getResource(url) {
+    const response = await fetch(`${this._apiBase}${url}`);
+
+    if (!response.ok) {
+      throw new Error(`Could not fetch ${url}, recieved status: ${response.status}`);
+    }
+
+    return response.json();
+  }
+
+  async getAllCharacters() {
+    const allCharacters = await this.getResource("/characters/");
+    return allCharacters.map(this._transformCharacter);
+  }
+
+  async getCharacterById(id) {
+    const character = await this.getResource(`/characters/${id}`);
+    return this._transformCharacter(character);
+  }
+
+  async getAllHouses() {
+    const allHouses = await this.getResource("/houses/");
+    return allHouses.map(this._transformHouse);
+  }
+
+  async getHouseById(id) {
+    const house = await this.getResource(`/houses/${id}`);
+    return this._transformHouse(house);
+  }
+
+  async getAllBooks() {
+    const allBooks = await this.getResource("/books/");
+    return allBooks.map(this._tranformBook);
+  }
+
+  async getBookById(id) {
+    const book = await this.getResource(`/books/${id}`);
+    return this._tranformBook(book);
+  }
+
+  _transformCharacter(character) {
+    return {
+      name: character.name || "unknown",
+      gender: character.gender || "unknown",
+      born: character.born || "unknown",
+      died: character.died || "unknown",
+      culture: character.culture || "unknown",
+    }
+  }
+
+  _transformHouse(house) {
+    return {
+      name: house.name || "unknown",
+      region: house.region || "unknown",
+      words: house.words || "unknown",
+      titles: house.titles || "unknown",
+      overlord: house.overlord || "unknown",
+      ancestralWeapons: house.ancestralWeapons || "unknown",
+    }
+  }
+
+  _tranformBook(book) {
+    return {
+      name: book.name || "unknown",
+      numberOfPages: book.numberOfPages || "unknown",
+      publiser: book.publiser || "unknown",
+      released: book.released || "unknown",
+    }
+  }
+}
