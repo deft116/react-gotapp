cdc9d49ee8a668e7c4147e8b27f8afab379f6ba3
Кирилюк <kk87.work@gmail.com>
2020-09-12 08:34:17 +0300

    add button for random char

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,34 +1,38 @@
-import React from 'react';
-import {Col, Row, Container} from 'reactstrap';
-import Header from '../header';
-import RandomChar from '../randomChar';
-import ItemList from '../itemList';
-import CharDetails from '../charDetails';
-
+import React, { useState } from "react";
+import { Col, Row, Container, Button } from "reactstrap";
+import Header from "../header";
+import RandomChar from "../randomChar";
+import ItemList from "../itemList";
+import CharDetails from "../charDetails";
 
 const App = () => {
-    return (
-        <> 
-            <Container>
-                <Header />
-            </Container>
-            <Container>
-                <Row>
-                    <Col lg={{size: 5, offset: 0}}>
-                        <RandomChar/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md='6'>
-                        <ItemList />
-                    </Col>
-                    <Col md='6'>
-                        <CharDetails />
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    );
+  const [isRandomCharacter, setRandomCharacter] = useState(true);
+  const showRandomCharacter = () => setRandomCharacter(!isRandomCharacter);
+	const randomChar = isRandomCharacter ? <RandomChar/> : null;
+	
+  return (
+    <>
+      <Container>
+        <Header />
+      </Container>
+      <Container>
+        <Row>
+          <Col lg={{ size: 5, offset: 0 }}>
+            {randomChar}
+						<Button color="secondary" onClick={showRandomCharacter}>secondary</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col md="6">
+            <ItemList />
+          </Col>
+          <Col md="6">
+            <CharDetails />
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 };
 
 export default App;
